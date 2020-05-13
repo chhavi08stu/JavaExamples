e9378b89dccc1becb46b8acdfca8bb53ebc51809
Garima Garg <chhavi08stu@gmail.com>
2020-05-14 00:32:41 +0530

    Update SlidingWindowMaximum.java

--- SlidingWindowMaximum.java
+++ SlidingWindowMaximum.java
@@ -1,22 +1,44 @@
 public class SlidingWindowMaximum {
     public static void main(String[] args) {
-        int[] arr={1,3,-1,-3,5,3,6, 7};
-            int winSize=3;
-        for(int i=0;i<arr.length-2;i++){
-            int[] tempA={arr[i],arr[i+1],arr[i+2]};
+int[] arr={1,3,-1,-3,5,3,6, 7};
+    int winSize=3;
+   int[] arrMax=getMax(arr,winSize);
+        System.out.println("*****************************************");
+display(arrMax);
+    }
+    private static int[] getMax(int[] A,int B){
 
-            int maxValue=getMaxValueFromSortedArray(tempA);
-            System.out.print(maxValue+" ");
+        List<Integer> list=new ArrayList<>();
+        int[] tempA=new int[B];
+
+                for(int i=0;i<A.length-B+1;i++){
+
+            int tempi=0;
+             for(int j=i;j<i+B;j++){
+
+            tempA[tempi]=A[j];
+                 tempi++;
+               }
+            int retMax=getMaxValueFromSortedArray(tempA);
+            list.add(retMax);
+
         }
-        System.out.println("");
+        int[] array = list.stream().mapToInt(i->i).toArray();
+        return array;
     }
     private static int getMaxValueFromSortedArray(int[] tempA){
-
+        display(tempA);
         int maxValue=0;
         for(int j=0;j<tempA.length;j++){
             Arrays.sort(tempA);
-            maxValue=tempA[tempA.length-1];
+             maxValue=tempA[tempA.length-1];
+                   }
+            return maxValue;
+    }
+    private static void display(int[] arr){
+        for (int i=0;i<arr.length;i++){
+            System.out.print(" "+arr[i]);
         }
-        return maxValue;
+        System.out.println("");
     }
 }
