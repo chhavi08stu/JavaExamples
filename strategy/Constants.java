6d647b14524f7c4278378279ec34c85fb17eed95
Garima Garg <chhavi08stu@gmail.com>
2020-08-20 14:18:47 +0530

    Add files via upload

---
+++ strategy/Constants.java
@@ -0,0 +1,7 @@
+package com.stest.companies.neebal.strategy;
+
+public class Constants {
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+}
---
+++ strategy/MainClass.java
@@ -0,0 +1,37 @@
+package com.stest.companies.neebal.strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainClass {
+	/*public static void main(String[] args) {
+
+		RentalCalculationClass rent = new RentalCalculationClass();
+		insertData(rent);
+		// Rental calculation
+		String s = rent.statement();
+		System.out.println("s: " + s);
+
+		// Report generation
+		ReportInterface ir = new ReportHTML();
+		ir.printReport(s);
+		
+		 If we want to generate PDF report then we can call PDF Report class
+		ir = new ReportPDF();
+		ir.printReport(s);
+	}
+
+	 Insert data into class before testing the problem statement 
+	private static void insertData(RentalCalculationClass rent) {
+
+		Movie movie1 = new Movie("ABC", 1);
+		List<Rental> list = new ArrayList<Rental>();
+		Rental rental = new Rental(movie1, 10);
+		list.add(rental);
+
+		Customer customer1 = new Customer("ABC", list);
+		rent.setCustomer(customer1);
+		rent.setMovie(movie1);
+
+	}*/
+}
