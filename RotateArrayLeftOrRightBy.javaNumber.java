d87aae5fd9e5b7219d8647571e8bc28a9b4df572
Garima Garg <chhavi08stu@gmail.com>
2021-10-10 01:29:27 +0530

    Update RotateArrayLeftOrRightBy.javaNumber.java

--- RotateArrayLeftOrRightBy.javaNumber.java
+++ RotateArrayLeftOrRightBy.javaNumber.java
@@ -1,17 +1,42 @@
 public class RotateByANumber {
     public static void main(String[] args) {
-        int[] arr=new int[]{2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-        rotateLeftByNum(arr,2);
-        rotateRightByNum(arr,2);
+        Integer[] arr={2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int k=2;
+        int n=arr.length;
+        rotateLeftByNum(arr,k); //Complexity O(n2)
+        rotateRightByNum(arr,k);// Complexity O(n2)
+        
+        rotateLeft(arr,n,k);
+        rotateRight(arr,n,k);
     }
-    private static void rotateLeftByNum(int[] arr,int num){
+    *********************************************************               Complexity O(n)                ************************************************************
+    private void  rotateLeft(Integer[] arr,int n,int k){
+           Collections.rotate(Arrays.asList(arr), n - k);
+		// print the array
+        for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+    }
+         private void  rotateRight(Integer[] arr,int n,int k){
+             Collections.rotate(Arrays.asList(arr), k);
+		// print the array
+        for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+    }
+        
+        
+        
+    *********************************************************               Complexity O(n2)                ************************************************************
+    
+    private static void rotateLeftByNum(int[] arr,int k){
         System.out.println("\n");
         System.out.println("******************************************************");
 
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+"\t");
         }
-        for(int i=0;i<num;i++){
+        for(int i=0;i<k;i++){
             arr=rotateLeft(arr);
         }System.out.println("\n");
         System.out.println("******************************************************");
